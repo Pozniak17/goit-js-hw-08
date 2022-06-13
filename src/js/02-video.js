0b355fe0d4ad3d0038c09beb17f6069a72ecd29c
Yevhenii Pozniak <jekamanu@gmail.com>
2022-06-13 15:17:41 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+// lodash.throttle
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+  // Виводимо таймінг в консоль
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const onSetup = function () {
+  player.setCurrentTime(
+    Number(localStorage.getItem('videoplayer-current-time'))
+  );
+};
+window.onload = onSetup;
