7f83a11705bcdeac73b675dbdc23270b796a6fef
Yevhenii Pozniak <jekamanu@gmail.com>
2023-03-26 11:27:32 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,12 +10,13 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
-populateTextarea();
+// populateTextarea();
+load(LOCALSTORAGE_KEY);
 
 function onFormSubmit(evt) {
   // блокуємо стандартні дії браузера
   evt.preventDefault();
-  // скидаємо нашу форму при відправці
+  // скидаємо нашу форму при відправціюванні
   evt.currentTarget.reset();
   //! при відправці форми видаляємо дані з localeStorage, щоб при наступному разі,
   //! текст не з'являвся з localeStorage в текстовому полі
@@ -27,21 +28,33 @@ function onTextareaInput(evt) {
   // значення інпуту
   // тут target, тому що при вик. currentTarget + throttle, в нас спливання і буде null,
   // з null ми не зможемо взяти value через 500 мс.
-  // const message = evt.target.value;
+  const message = evt.target.value;
   // значення інпута записуємо в localeStorage
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(message));
 }
 
 // При завантажені сторінки, витягуємо збережені дані з localeStorage
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
-  // якщо в localeStorage немає ніяких даних, то в консолі буде null
-  // якщо є ключ в localeStorage, то виводимо в консоль, якщо немає - нічого не робимо
-  if (savedMessage) {
-    console.log(savedMessage);
-    // якщо щось є - оновлюємо наш DOM, таким чином, при перезавантажені сторінки
-    // залишаються дані які вводив користувач з localeStorage, в текстовому полі
-    refs.textarea.value = savedMessage;
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+//   // якщо в localeStorage немає ніяких даних, то в консолі буде null
+//   // якщо є ключ в localeStorage, то виводимо в консоль, якщо немає - нічого не робимо
+//   if (savedMessage) {
+//     console.log(JSON.parse(savedMessage));
+//     // якщо щось є - оновлюємо наш DOM, таким чином, при перезавантажені сторінки
+//     // залишаються дані які вводив користувач з localeStorage, в текстовому полі
+//     refs.textarea.value = JSON.parse(savedMessage);
+//   }
+// }
+
+//! замінив цю функцію вище
+function load(key) {
+  try {
+    const savadMessege = localStorage.getItem(key);
+    return savadMessege === null
+      ? undefined
+      : (refs.textarea.value = JSON.parse(savadMessege));
+  } catch (error) {
+    console.error('Get state error: ', error.message);
   }
 }
 
